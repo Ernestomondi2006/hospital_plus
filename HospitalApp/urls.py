1fed540d1d391e72c483132094aaf378a543a795
Ernestomondi2006 <ernestomondi2006@gmail.com>
2025-03-05 11:02:40 +0300

    initial commit

--- HospitalApp/urls.py
+++ HospitalApp/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('contact/', views.contacts, name='contact'),
     path('show/', views.show, name='show'),
     path('delete/<int:id>', views.delete, name='show'),
+    path('edit/<int:id>', views.edit, name='edit'),
 
 
 
